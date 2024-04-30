f1ccfcc60c486e657f13425c1e67378b8d1a517a
StormXDeV <166788469+StormXDeV@users.noreply.github.com>
2024-04-30 06:39:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,75 +1,95 @@
-const mineflayer = require('mineflayer')
-const cmd = require('mineflayer-cmd').plugin
+const mineflayer = require('mineflayer');
+const cmd = require('mineflayer-cmd').plugin;
 const fs = require('fs');
-const keep_alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js');
+
 let rawdata = fs.readFileSync('config.json');
 let data = JSON.parse(rawdata);
+
 var lasttime = -1;
 var moving = 0;
 var connected = 0;
-var actions = [ 'forward', 'back', 'left', 'right']
+var actions = ['forward', 'back', 'left', 'right'];
 var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
-var username = data["name"]
-var nightskip = data["auto-night-skip"]
-var bot = mineflayer.createBot({
-  host: host,
-  username: username
-});
-function getRandomArbitrary(min, max) {
-       return Math.random() * (max - min) + min;
+var username = data["name"];
+var nightskip = data["auto-night-skip"];
 
+var bot;
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
-bot.loadPlugin(cmd)
+function createBot() {
+    bot = mineflayer.createBot({
+        host: host,
+        username: username
+    });
 
+    bot.loadPlugin(cmd);
 
+    bot.on('login', function() {
+        console.log("Logged In");
+        bot.chat("hello");
+    });
 
-bot.on('login',function(){
-	console.log("Logged In")
-	bot.chat("hello");
-});
-
-bot.on('time', function(time) {
-	if(nightskip == "true"){
-	if(bot.time.timeOfDay >= 13000){
-	bot.chat('/time set day')
-	}}
-    if (connected <1) {
-        return;
-    }
-    if (lasttime<0) {
-        lasttime = bot.time.age;
-    } else {
-        var randomadd = Math.random() * maxrandom * 20;
-        var interval = moveinterval*20 + randomadd;
-        if (bot.time.age - lasttime > interval) {
-            if (moving == 1) {
-                bot.setControlState(lastaction,false);
-                moving = 0;
-                lasttime = bot.time.age;
-            } else {
-                var yaw = Math.random()*pi - (0.5*pi);
-                var pitch = Math.random()*pi - (0.5*pi);
-                bot.look(yaw,pitch,false);
-                lastaction = actions[Math.floor(Math.random() * actions.length)];
-                bot.setControlState(lastaction,true);
-                moving = 1;
-                lasttime = bot.time.age;
-                bot.activateItem();
+    bot.on('time', function(time) {
+        if (nightskip == "true") {
+            if (bot.time.timeOfDay >= 13000) {
+                bot.chat('/time set day');
             }
         }
-    }
-});
 
-bot.on('spawn',function() {
-    connected=1;
-});
+        if (connected < 1) {
+            return;
+        }
 
-bot.on('death',function() {
-    bot.emit("respawn")
-});
+        if (lasttime < 0) {
+            lasttime = bot.time.age;
+        } else {
+            var randomadd = Math.random() * maxrandom * 20;
+            var interval = moveinterval * 20 + randomadd;
+            if (bot.time.age - lasttime > interval) {
+                if (moving == 1) {
+                    bot.setControlState(lastaction, false);
+                    moving = 0;
+                    lasttime = bot.time.age;
+                } else {
+                    var yaw = Math.random() * pi - (0.5 * pi);
+                    var pitch = Math.random() * pi - (0.5 * pi);
+                    bot.look(yaw, pitch, false);
+                    lastaction = actions[Math.floor(Math.random() * actions.length)];
+                    bot.setControlState(lastaction, true);
+                    moving = 1;
+                    lasttime = bot.time.age;
+                    bot.activateItem();
+                }
+            }
+        }
+    });
 
+    bot.on('spawn', function() {
+        connected = 1;
+    });
+
+    bot.on('death', function() {
+        bot.emit("respawn");
+    });
+
+    bot.on('kicked', function(reason, loggedIn) {
+        if (loggedIn) {
+            console.log("Bot was kicked from the server. Reconnecting...");
+            setTimeout(createBot, 5000); // Reconnect after 5 seconds
+        } else {
+            console.log("Failed to login. Reconnecting...");
+            setTimeout(createBot, 5000); // Reconnect after 5 seconds
+        }
+    });
+}
+
+// Initialize the bot
+createBot();
